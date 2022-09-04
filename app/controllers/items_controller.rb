bc01cce947b30471e5d7949499a3dc7512480777
Tomer Harari <tharari21@gmail.com>
2022-09-04 13:22:29 -0400

    commit tree 708e76b0eaa5ea548603ab158219b431cd494322

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item
+  end
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: :created
+  end
+  private
+  def render_not_found
+    render json: {error: "User not found"}, status: :not_found
+  end
+  def item_params
+    params.permit(:name,:description, :price, :user_id)
+  end
 end
